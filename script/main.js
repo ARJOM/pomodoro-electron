e94f9810fa72604c7e5f555be62b121469cda368
Ricart <ricarteusuo11@gmail.com>
2020-02-11 10:45:37 -0300

    atualizando exibição do cronometro

--- script/main.js
+++ script/main.js
@@ -1,12 +1,43 @@
+minutos = 25;
+segundos = 10;
 var inter;
-function iniciar(){
-   inter =  setInterval(function(){ 
-        localDate = new Date();
-        var hora = document.getElementById('interval'). value = localDate.toLocaleTimeString();
-        hora.toLocaleTimeString();
-    },
-    1000);
+
+function load(){
+    document.getElementById("interval").value =  minutos+":"+segundos;
 }
-function parar(){
+
+function iniciar(){
+    document.getElementById("start").disabled = true;
+    inter = setInterval(function(){
+        crono = document.getElementById("crono");
+
+        if (segundos == 0){
+            minutos -= 1;
+            segundos = 60; 
+        }
+        segundos -= 1;
+
+        if(segundos/10<1){
+            segundos = "0"+segundos;
+        }
+        if(minutos/10<1){
+            minutos = "0"+minutos;
+        }
+
+        document.getElementById("interval").value =  minutos+":"+segundos;
+
+        segundos = parseInt(segundos);
+        minutos = parseInt(minutos);
+
+    }, 1000);
+}
+
+function pausar(){
     clearInterval(inter);
+}
+
+function zerar(){
+    minutos = 25;
+    segundos = 0; 
+    document.getElementById("interval").value =  minutos+":"+segundos;
 }
